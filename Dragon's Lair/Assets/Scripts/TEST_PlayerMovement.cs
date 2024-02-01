db7be2efaa6bbc757ffcb9a638fa8e11f82cdd19
carlosdmtz13 <carlosdmtz13@gmail.com>
2024-02-01 15:27:35 -0600

    Created a Test Scene to Test Interactor Code

---
+++ Dragon's Lair/Assets/Scripts/TEST_PlayerMovement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEST_PlayerMovement : MonoBehaviour
+{
+    public float horizontalInput;
+    public float verticalInput;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * verticalInput);
+        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput);
+    }
+}
