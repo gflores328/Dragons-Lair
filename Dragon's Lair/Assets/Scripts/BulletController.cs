685c5282cd0fde23b95a113fbcb1449c6473ef82
carlosdmtz13 <carlosdmtz13@gmail.com>
2024-02-09 01:49:53 -0600

    Created a TEST_Projectile Scene
    - TEST_Projectile scene created to test projectiles
    - Created scripts for Gun Controller and Bullet Controller

---
+++ Dragon's Lair/Assets/Scripts/BulletController.cs
@@ -0,0 +1,51 @@
+/*
+ * Created by Carlos Martinez
+ * 
+ * This script contains the controller for the bullet.
+ * It can manipulate the speed of the bullet and call the Gun Controller.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public float speed = 15; // Movement speed of the bullet (Default = 15)
+
+    private ParticleSystem part; // Particle System
+    private List<ParticleCollisionEvent> collisionEvents = new List<ParticleCollisionEvent>(); // For particle collision
+
+    public GunController gun; // Gun Controller
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        part = GetComponent<ParticleSystem>(); // Calls particle system
+    }
+    
+    // For when particles collide
+    private void OnParticleCollision(GameObject other)
+    {
+        // If particle hits an enemy
+        if (other.CompareTag("Enemy"))
+        {
+            Debug.Log("Hit " + other.gameObject); // Debug message
+        }
+    }
+
+    // Affects the firing of the bullet
+    public void Fire(int shots)
+    {
+        gun.shotsFired++; // Raises counter for shots fired
+        part.Emit(shots); // Fires bullet
+
+        // Accuracy
+        var partS = part.shape;
+        partS.angle = (100 - gun.accuracy);
+        
+        // Bullet speed 
+        var partM = part.main;
+        partM.startSpeed = speed;
+    }
+}
