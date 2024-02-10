d388bd6f275643ac909b72dde3f00e676e1576ef
jleatherman3 <77819727+jleatherman3@users.noreply.github.com>
2024-02-09 21:18:14 -0600

    Need to troubleshoot issue with ledges disappearing and reappearing incorrectly.

---
+++ Dragon's Lair/Assets/Scripts/JMLTest_DisappearingLedges.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JMLTest_DisappearingLedges : MonoBehaviour
+{
+    [SerializeField] private GameObject obj;
+    //[SerializeField] private int speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter()
+    {
+        StartCoroutine(Vanish());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine(Vanish());  //makes platform vanish on its own without player interaction
+    }
+
+    IEnumerator Vanish()
+    {
+        for (; ; )
+        {
+            obj.SetActive(true);
+            yield return new WaitForSeconds(5);
+
+            obj.SetActive(false);
+            yield return new WaitForSeconds(2);
+
+        }
+    }
+}
