357770efc650c3bacc5ba084cfaf0dbd29954c90
Gabriel Flores <gravy239@gmail.com>
2024-02-14 21:53:47 -0600

    Added new type of Dialogue asset
    Added a new type of dialogue asset that will replace the old one. This new one uses a struct array that includes two strings far the dialogue and a name.

---
+++ Dragon's Lair/Assets/Scripts/DialogueWithName.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct DialogueAndName
+{
+    [Tooltip("The dialogue line")]
+    [TextArea]
+    public string dialogue;
+    [Tooltip("The name of who or what said the dialogue line")]
+    public string name;
+}
+
+[CreateAssetMenu]
+public class DialogueWithName : ScriptableObject
+{
+    public DialogueAndName[] dialogue;
+}
