cde816b518abd88f3a22e7b8abd1c7c3a2b37e9a
jleatherman3 <77819727+jleatherman3@users.noreply.github.com>
2024-02-19 14:15:50 -0600

    Spike trap mostly done. Need help fixing damage output. It seems to be instant killing the player upon impact now (might be interferance from another script).
    Added Spike animation and controller as well as activation script to run the animations. When player reaches the trap area they seem to instantly get killed rather than have the animation play and let them continue. _JML

---
+++ Dragon's Lair/Assets/Scripts/Activation.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Activation : MonoBehaviour
+{
+    //public float damage;
+
+    void Start()
+    {
+       
+
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            transform.GetComponent<Animator>().Play("Spike");
+
+            //start damage functions here
+        }
+    }
+}
