e1631d9d2504aec2f9e40b07d0dde3355019e929
jleatherman3 <77819727+jleatherman3@users.noreply.github.com>
2024-02-20 13:26:51 -0600

    Finished spike trap prefab JML

--- Dragon's Lair/Assets/Scripts/Activation.cs
+++ Dragon's Lair/Assets/Scripts/Activation.cs
@@ -1,13 +1,22 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class Activation : MonoBehaviour
 {
     //public float damage;
 
+    private Animator anim;
+
+    public float waitTime;
+
+    private IEnumerator coroutine;
+
     void Start()
     {
-       
 
+        anim = GetComponent<Animator>();
+        coroutine = WaitTime(waitTime);
+        StartCoroutine(coroutine);
     }
 
     void Update()
@@ -16,13 +25,29 @@ public class Activation : MonoBehaviour
     }
 
 
-    private void OnTriggerStay(Collider other)
+    public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            transform.GetComponent<Animator>().Play("Spike");
+            
+            anim.SetTrigger("onActive");
+            coroutine = WaitTime(waitTime);
+            StartCoroutine(coroutine);
 
-            //start damage functions here
         }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            anim.StopPlayback();
+        }
+    }
+
+    private IEnumerator WaitTime(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        
     }
 }
