031e23c98cb3729c30a8baabce6f9649171b823c
jleatherman3 <77819727+jleatherman3@users.noreply.github.com>
2024-02-25 11:15:56 -0600

    Update AH_Controller still WIP

--- Dragon's Lair/Assets/Scripts/AH_Controller.cs
+++ Dragon's Lair/Assets/Scripts/AH_Controller.cs
@@ -1,13 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AH_Controller : MonoBehaviour
 {
+    //this script controls the movement for the players in the Air Hockey Game
+
+    private Rigidbody rb;
+
+    public float speed = 5;
+    public float minDist = 0;
+    public float maxDist = 5;
+    public LayerMask layers;
+    
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
