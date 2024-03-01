5cdc08c7ada1340e987905516a0a0934a66be894
jleatherman3 <77819727+jleatherman3@users.noreply.github.com>
2024-03-01 09:46:56 -0600

    Update AH_Controller.cs

--- Dragon's Lair/Assets/Scripts/AH_Controller.cs
+++ Dragon's Lair/Assets/Scripts/AH_Controller.cs
@@ -4,13 +4,13 @@ public class AH_Controller : MonoBehaviour
 {
     //this script controls the movement for the players in the Air Hockey Game
 
-    public float speed = 15;
-    public float minDist = 0;
-    public float maxDist = 10;
-    public LayerMask layers;
-    public Rigidbody myRigidBody;
+    public float speed = 15; //speed the object can move
+    public float minDist = 0; //minimum distance
+    public float maxDist = 10; //max distance
+    public LayerMask layers;  //layerMask to have raycast go directly to ground
+    public Rigidbody myRigidBody; //rigidbody of player object
     
-    [SerializeField] private Camera mainCamera;
+    [SerializeField] private Camera mainCamera;  //main camera object
 
 
     // Start is called before the first frame update
@@ -29,7 +29,7 @@ public class AH_Controller : MonoBehaviour
     void Update()
     {
 
-
+        //casts a ray to the location of the mouse
         Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out RaycastHit hit,layers))
         {
