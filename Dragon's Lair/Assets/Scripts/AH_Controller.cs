84719ca1f7410db18384c9f13a6cf2cc7f003c5e
jleatherman3 <77819727+jleatherman3@users.noreply.github.com>
2024-03-01 20:39:57 -0600

    Worked with Gabriel on AH_Controller and physics problems inside the AirHockey Game. We now have the Player colliding with the puck and are working on ajusting the puck speed and travel patterns and distance before working on the AI.

--- Dragon's Lair/Assets/Scripts/AH_Controller.cs
+++ Dragon's Lair/Assets/Scripts/AH_Controller.cs
@@ -4,10 +4,11 @@ public class AH_Controller : MonoBehaviour
 {
     //this script controls the movement for the players in the Air Hockey Game
 
-    public float speed = 15; //speed the object can move
+    public float speed = 30; //speed the object can move
     public float minDist = 0; //minimum distance
-    public float maxDist = 10; //max distance
+    public float maxDist = 30; //max distance
     public LayerMask layers;  //layerMask to have raycast go directly to ground
+    private Rigidbody rb;
    
     
     [SerializeField] private Camera mainCamera;  //main camera object
@@ -16,7 +17,8 @@ public class AH_Controller : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponentInChildren<Rigidbody>();
+      
     }
 
     private void Awake()
@@ -34,10 +36,10 @@ public class AH_Controller : MonoBehaviour
 
         if (Physics.Raycast(ray, out RaycastHit hit,maxDist, layers))
         {
-            transform.position = hit.point;
-            
 
-            
+            rb.MovePosition(Vector3.MoveTowards(transform.position, hit.point, speed * Time.deltaTime));
+
+
         }
 
     }
