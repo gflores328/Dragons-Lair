a4350e8d6b31dcbdd082df11d452ae93d50d36b2
jleatherman3 <77819727+jleatherman3@users.noreply.github.com>
2024-03-05 12:02:36 -0600

    Testing new script for AH_Puck

---
+++ Dragon's Lair/Assets/Scripts/AH_Puck.cs
@@ -0,0 +1,27 @@
+
+using UnityEngine;
+
+
+public class AH_Puck : MonoBehaviour
+{
+    public Rigidbody myRB;
+    public float speed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponentInChildren<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        myRB.AddForce(speed, 0, 0, ForceMode.Acceleration);
+    }
+}
