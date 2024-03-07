940488fc385f1e0ee62b6501a4aef413371fbca0
jleatherman3 <77819727+jleatherman3@users.noreply.github.com>
2024-03-06 21:58:11 -0600

    Changing AH_Table and controls. Will start on scripts and scroing tomorrow

---
+++ Dragon's Lair/Assets/Scripts/ChangeCursor.cs
@@ -0,0 +1,27 @@
+
+//JML - Helps to change cursor in a scene. Add to empty game object called cusor and put cusor image in the cursor of inspector.
+
+using UnityEngine;
+
+public class setCursor : MonoBehaviour
+{
+    // You must set the cursor in the inspector.
+    public Texture2D CustomCursor;
+    public Texture2D ClickCursor;
+
+    void Start()
+    {
+
+        //set the cursor origin to its centre. (default is upper left corner)
+        Vector2 cursorOffset = new Vector2(CustomCursor.width / 2, CustomCursor.height / 2);
+
+        //Sets the cursor to the Crosshair sprite with given offset 
+        //and automatic switching to hardware default if necessary
+        Cursor.SetCursor(CustomCursor, cursorOffset, CursorMode.Auto);
+    }
+
+    void OnMouseOver()
+    {
+        Cursor.SetCursor(ClickCursor, Vector2.zero, CursorMode.Auto);
+    }
+}
