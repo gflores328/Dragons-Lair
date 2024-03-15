4f754b6c59dbc610d5410205e9e1617ae9713807
Aaron Torres <deadkilla67@gmail.com>
2024-03-15 03:29:15 -0500

    Added instructions at the start of level one
    The scale for the images def needs work on and the text flips with player and needs to be fixed created a script to handle it

--- Dragon's Lair/Assets/Scripts/ChibiPlayerMovement.cs
+++ Dragon's Lair/Assets/Scripts/ChibiPlayerMovement.cs
@@ -27,6 +27,9 @@ public class ChibiPlayerMovement : MonoBehaviour
     private InputAction pauseAction; // A private variable that holds the pause action
 
     private InputAction freeAimAction; // A private variable that holds the free aim action 
+
+    private InputAction freeAimLeftAction; // A private variable meant to hold the free aim left action
+    private InputAction freeAimRightAction; // A private variable meant to hold the free aim left action
     private GameObject gameManagerObj; // A gameObject variable that will hold the game manager game object
     private GameManager gameManager; // A GameManger object that will hold the instance of the script of GameManger
     
@@ -55,6 +58,8 @@ public class ChibiPlayerMovement : MonoBehaviour
     private Rigidbody playerRB; // A rigid body object which will hold the player's rigid body
     private playerState currentPlayerState; // the state that will hold the players current state by using the playerState enum created below
     private bool isGrounded; // A bool to know if the player is grounded
+
+    private bool isFacingRight; // A bool to determine which way the character is facing right
 
   
     //PlayerUI and Information Section
@@ -97,9 +102,14 @@ public class ChibiPlayerMovement : MonoBehaviour
         pauseAction.performed += Pause; // Assigns the on performed pause action to the pause function 
         gameManager = FindObjectOfType<GameManager>(); // Finds the game manger in the scene
         freeAimAction = playerInput.actions.FindAction("FreeAim");
+        freeAimLeftAction = playerInput.actions.FindAction("FreeAimLeft");
+        freeAimRightAction = playerInput.actions.FindAction("FreeAimRight");
+
 
         freeAimAction.performed += OnFreeAimPerformed;
         freeAimAction.canceled += OnFreeAimCanceled;
+        freeAimLeftAction.performed += OnFreeAimLeft;
+        freeAimRightAction.performed += OnFreeAimRight;
         //setupJumpVariables();
 
     }
@@ -173,7 +183,7 @@ public class ChibiPlayerMovement : MonoBehaviour
         if (direction.x > 0) // Moving right
 
         {
-
+            isFacingRight = true;
             transform.rotation = Quaternion.Euler(0, 0, 0);
 
         }
@@ -181,7 +191,7 @@ public class ChibiPlayerMovement : MonoBehaviour
         else if (direction.x < 0) // Moving left
 
         {
-
+            isFacingRight = false;
             transform.rotation = Quaternion.Euler(0, 180, 0); // Flipped scale along x-axis
             
         }
@@ -215,7 +225,7 @@ public class ChibiPlayerMovement : MonoBehaviour
     void handleJump()
     {
 
-        if(!isJumping && coyoteTimeCounter > 0 && isJumpingPressed)
+        if(!isJumping && coyoteTimeCounter > 0 && isJumpingPressed && !playerOneWay.IsDownActionActive())
         {
             isJumping = true;
             playerRB.velocity = new Vector3(playerRB.velocity.x, initialJumpVelocity, 0);
@@ -236,6 +246,7 @@ public class ChibiPlayerMovement : MonoBehaviour
         if(isGrounded)
         {
             playerRB.AddForce(Vector3.down * gravityForce); // Push the player in the down direction by adding force down
+            StopPlayerMovement();
         }
 
         else if(isFalling)
@@ -337,11 +348,36 @@ public class ChibiPlayerMovement : MonoBehaviour
     private void OnFreeAimPerformed(InputAction.CallbackContext value) // The pause function that is called when the button is pressed
     {
         isFreeAiming = true;
+        StopPlayerMovement();
     }
 
      private void OnFreeAimCanceled(InputAction.CallbackContext value) // The pause function that is called when the button is pressed
     {
         isFreeAiming = false;
     }
-  
+    
+
+    private void OnFreeAimLeft(InputAction.CallbackContext value) // The function that is called when freeaiming and trying to face left
+    {
+        if(isFacingRight)
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0); // Flip player to face left
+            isFacingRight = false;
+        }
+    }
+
+    private void OnFreeAimRight(InputAction.CallbackContext value) // The function that is called when free aiming and trying to face right
+
+    {
+        if(!isFacingRight)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0); // Rotates the character to face the right
+            isFacingRight = true;
+        }
+    }
+
+    private void StopPlayerMovement()
+    {
+        playerRB.velocity = Vector3.zero;
+    }
 }
