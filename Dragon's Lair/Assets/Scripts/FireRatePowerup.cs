5711c3a41bdac6a6ca46e7441499fe6eb7f3af89
Aaron Torres <deadkilla67@gmail.com>
2024-03-18 17:45:48 -0500

    Updated some more level design things added instructions to the fire rate powerup
    fixed why the pixies weren't working redesigned some level 3 to adjust with the different move system added traps and changed the spikes to always be popping up and down

--- Dragon's Lair/Assets/Scripts/FireRatePowerup.cs
+++ Dragon's Lair/Assets/Scripts/FireRatePowerup.cs
@@ -4,32 +4,43 @@ using UnityEngine;
 
 public class FireRatePowerup : MonoBehaviour
 {
-    public float fireRateSetter; // float for what rate the powerup will set the fire rate to
-    //public GameObject powerUpDesc; // A game object variable that will hold the words that appear above the players head and tells it what the powerup does
-
-    private GunController gunController; // A private gun controller variable to grab the player's gun controller script
+    public float fireRateSetter; 
+    public GameObject powerUpDesc; 
+    private GunController gunController; 
+    private bool showDesc = false;
+    private float waitTime = 5f;
+    private IEnumerator coroutine;
 
     void Awake()
     {
-        gunController = FindObjectOfType<GunController>(); // On awake it finds the gun controller ready to access it
+        gunController = FindObjectOfType<GunController>(); 
+        coroutine = WaitAndDisable(waitTime);
     }
+
+    void Update()
+    {
+        if (showDesc)
+        {
+            powerUpDesc.SetActive(true);
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {   
-            //powerUpDesc.SetActive(true);
-            gunController.setFireRate(fireRateSetter); // calls the setfire rate function from the gun controller script
-            Destroy(gameObject); // Destroys the powerup on pick up
+            showDesc = true;
+            gunController.setFireRate(fireRateSetter);
+            StartCoroutine(coroutine);
         }
     }
 
-    private void OnTriggerExit(Collider other)
-
+    private IEnumerator WaitAndDisable(float waitTime)
     {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            //powerUpDesc.SetActive(false);
-            Debug.Log("I have exited");
-        }
+        yield return new WaitForSeconds(waitTime);
+        showDesc = false;
+        powerUpDesc.SetActive(false);
+        Destroy(gameObject);
     }
 }
+
