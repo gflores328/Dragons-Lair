bcbcd00d56c4736897fcb7731fcf3521879484be
TrevorMinarikNVC <157077764+TrevorMinarikNVC@users.noreply.github.com>
2024-03-19 12:14:29 -0500

    Inventory update
    UI is broken though

--- Dragon's Lair/Assets/Scripts/Inventory.cs
+++ Dragon's Lair/Assets/Scripts/Inventory.cs
@@ -2,7 +2,7 @@
  * CREATED BY: Trevor Minarik
  * 
  * LAST MODIFIED BY: Trevor Minarik
- * LAST MODIFIED ON: Feb 16, 2024 at 10:01 AM
+ * LAST MODIFIED ON: Mar 16, 2024 at 11:28 PM
  * 
  * Manages a list of items by
  * - Adding items
@@ -32,36 +32,18 @@ public class Inventory : MonoBehaviour
     }
 
 
-    /*
-     * Adds the given item
-     * If an item with the same name already exists in the inventory, add the given item's quantity to the existing item
-     * If an item with the same name does NOT already exist in the inventory, add the new item
-     */
+    // Adds the given item and updates the Inventory UI
     public void AddItem(Item givenItem)
     {
-        //The quantity of the item being added must be a positive integer
-        if (givenItem.GetQuantity() > 0)
-        {
-            //Check to see if the item to be added already exists in the inventory
-            Item existingItem = FindItem(givenItem.GetName());
+        //Add the item
+        inventory.Add(givenItem);
+        //Update the Inventory UI
+        inventoryUI.UpdateUI(GetInventory());
 
-            //If the item isn't already in the inventory accept the given item
-            if (existingItem == null)
-            {
-                inventory.Add(givenItem);
-            }
-            //If the item does aready exist simply increase the quantity of the existing item
-            else
-            {
-                existingItem.IncreaseQuantity(givenItem.GetQuantity());
-            }
-
-            //Update the Inventory UI
-            inventoryUI.UpdateUI(GetInventory());
-        }
+        Debug.Log("Item added");
     }
 
-    //Removes a specific amount or all of a given item
+    //Removes the last instance of a given item
     private bool RemoveItem(Item givenItem, bool removeAll)
     {
         //A variable to keep track of whether the item has been removed or not
@@ -76,21 +58,24 @@ public class Inventory : MonoBehaviour
         {
             wasItemRemoved = false;
         }
-        //If the item does exist...
+        //If the item does exist, remove it
         else
         {
-            //Calculate the new quantity of the item
-            int newQuantity = existingItem.GetQuantity() - givenItem.GetQuantity();
-
-            //If the new quantity is less than one or if removeAll is true remove the item from the inventory completely
-            if (removeAll || newQuantity < 1)
+            if (removeAll)
             {
-                inventory.Remove(existingItem);
+                //Itterate through the inventory
+                for (int i = 0; i < inventory.Count; i++)
+                {
+                    //Remove every item with the same name
+                    if (inventory[i].name.Equals(existingItem.name))
+                    {
+                        inventory.Remove(existingItem);
+                    }
+                }
             }
-            //If the new quantity results in a postive integer simply decrease the quantity of the existing item
             else
             {
-                existingItem.DecreaseQuantity(givenItem.GetQuantity());
+                inventory.Remove(existingItem);
             }
 
             //Mark that the item was removed
@@ -107,22 +92,23 @@ public class Inventory : MonoBehaviour
     //Returns the inventory as an array of items
     public Item[] GetInventory()
     {
+        inventory.Sort();
         //Return the inventory
         return inventory.ToArray();
     }
 
-    //Returns an item with a name matching the given item name. Returns a null reference if no such item is found.
+    //Returns the most recently added item with a name matching the given item name. Returns a null reference if no such item is found.
     //Accepts a string for the item name.
     private Item FindItem(string itemName)
     {
         //Search through the inventory
-        foreach (Item i in inventory)
+        for (int i = inventory.Count; i > 0; i--)
         {
             //Compare the current item's name to the given name
-            if (i.GetName().Equals(itemName))
+            if (inventory[i].GetName().Equals(itemName))
             {
                 //If the item name matches, return the reference to the item
-                return i;
+                return inventory[i];
             }
         }
 
@@ -130,8 +116,7 @@ public class Inventory : MonoBehaviour
         return null;
     }
 
-    //Returns true if there is at least a specified quantity of an item in an inventory
-    //e.g. a quantity of 3 would return true if the item's quantity is 3 or above and return false for 2 and below
+    //Returns true if there is at least one item with the given item's name
     public bool Contains(Item newItem)
     {
         //Tracks whether the desired item exists in the inventory
@@ -142,11 +127,7 @@ public class Inventory : MonoBehaviour
         //First, check to make sure the item exists before trying to access its data
         if (item != null)
         {
-            //If the given item's quantity is greater than zero AND the found item's quantity is greater than the given item's quantity, mark the item as existing in the inventory
-            if (newItem.GetQuantity() > 0 && item.GetQuantity() >= newItem.GetQuantity())
-            {
-                containsItem = true;
-            }
+            containsItem = true;
         }
 
         //Return the state of the desired item existing or not
