e7358a3eec0a86ef02f634728008e318195db236
Aaron Torres <deadkilla67@gmail.com>
2024-03-21 01:28:50 -0500

    Added back in the mouse aiming added a bool that is able to switch between keyboard and mouse
    The default movement is mouse but if the bool is clicked then the mouse will be disabled and the keyboard will be enabled and planning to add the option into the settings

--- Dragon's Lair/Assets/Scripts/ArmRotation.cs
+++ Dragon's Lair/Assets/Scripts/ArmRotation.cs
@@ -15,6 +15,7 @@ public class ArmRotation : MonoBehaviour
 
     public float targetYRotation = 90f; // Target Y rotation
 
+    public bool gunRotationWithMouse = true; // A bool variable to determine if the player wants to use the mouse to aim or the keyboard
 
 
     private Vector2 lastMousePosition; // Last mouse position in screen space
@@ -84,36 +85,54 @@ public class ArmRotation : MonoBehaviour
         {
             // currentMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             // Debug.Log(currentMousePosition);
-            
-            Vector2 input = context.ReadValue<Vector2>(); // Read input value
-            if(input.y <= 1.0f && input.y >= 0.9f) // If the joy stick is going to the top  these are the parameters
+            if (context.control.device is Gamepad)
             {
-                armPivot.localRotation = Quaternion.Euler(-90f, 90f, 0f); // rotate the gun all the way up
+                Vector2 input = context.ReadValue<Vector2>(); // Read input value
+                if(input.y <= 1.0f && input.y >= 0.9f) // If the joy stick is going to the top  these are the parameters
+                {
+                    armPivot.localRotation = Quaternion.Euler(-90f, 90f, 0f); // rotate the gun all the way up
 
+                }
+                else if(input.y < 0.9f && input.y >= 0.5f) // If the joy stick is going to the top right or left
+                { 
+                    armPivot.localRotation = Quaternion.Euler(-45f, 90f, 0f); // rotate the gun to the top right or left 
+                }
+                
+                // else if(input.y >= -0.9f && input.y <= -0.5f) // if the joy stick is pointed to the bottom right or left 
+                // {
+                //     armPivot.localRotation = Quaternion.Euler(45f, 90f, 0f); // rotate the gun to the bottom right and left
+                // }
+                else
+                {
+                    
+                    armPivot.localRotation = Quaternion.Euler(0f, 90f, 0f); // Set default rotation
+                    
+                }
             }
-            else if(input.y < 0.9f && input.y >= 0.5f) // If the joy stick is going to the top right or left
-            { 
-                armPivot.localRotation = Quaternion.Euler(-45f, 90f, 0f); // rotate the gun to the top right or left 
-            }
-            
-            // else if(input.y >= -0.9f && input.y <= -0.5f) // if the joy stick is pointed to the bottom right or left 
-            // {
-            //     armPivot.localRotation = Quaternion.Euler(45f, 90f, 0f); // rotate the gun to the bottom right and left
-            // }
-            else
+
+
+            else if (context.control.device is Mouse && gunRotationWithMouse) // If not using a controller it must use the cursor
             {
-                
-                armPivot.localRotation = Quaternion.Euler(0f, 90f, 0f); // Set default rotation
-                
+                // Get the current mouse position
+                Vector3 mousePosition = Input.mousePosition;
+
+                // Convert the mouse position to a point in the game world
+                Vector3 targetPosition = mainCamera.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, transform.position.z - mainCamera.transform.position.z));
+
+                // Set the Z-axis of the target position to the current Z position of the arm pivot
+                targetPosition.z = armPivot.position.z;
+
+                // Rotate the gun towards the cursor position
+                RotateGun(targetPosition);
+            
             }
-            
-            
         }
     }
 
+    
     private void OnAimUpPerformed(InputAction.CallbackContext context)
     {
-        if (gameManager != null && gameManager.currentState == GameManager.pauseState.Unpaused)
+        if (gameManager != null && gameManager.currentState == GameManager.pauseState.Unpaused && !gunRotationWithMouse)
         {
             armPivot.localRotation = Quaternion.Euler(-90f, 90f, 0f); // rotate the gun all the way up
         }
@@ -123,7 +142,7 @@ public class ArmRotation : MonoBehaviour
     private void OnAimUpDiagPerformed(InputAction.CallbackContext context)
 
     {
-        if (gameManager != null && gameManager.currentState == GameManager.pauseState.Unpaused)
+        if (gameManager != null && gameManager.currentState == GameManager.pauseState.Unpaused && !gunRotationWithMouse)
         {
             armPivot.localRotation = Quaternion.Euler(-45f, 90f, 0f); // rotate the gun to the top right or left 
         }
@@ -132,7 +151,7 @@ public class ArmRotation : MonoBehaviour
     private void OnAimUpCanceled(InputAction.CallbackContext context)
 
     {
-        if (gameManager != null && gameManager.currentState == GameManager.pauseState.Unpaused)
+        if (gameManager != null && gameManager.currentState == GameManager.pauseState.Unpaused && !gunRotationWithMouse)
         {
            armPivot.localRotation = Quaternion.Euler(0f, 90f, 0f);
         }
@@ -148,5 +167,32 @@ public class ArmRotation : MonoBehaviour
     {
         // Do Crounching things
     }
-    
+    void RotateGun(Vector3 targetPosition)
+    {
+        // Calculate the direction to the target
+        Vector3 lookDirection = targetPosition - armPivot.position;
+
+
+
+
+
+        // Ignore the z-axis rotation
+        lookDirection.z = 0f;
+
+        if (lookDirection.magnitude > 0.001f)
+
+        {
+            // Calculate the angle to rotate the arm pivot around the X-axis
+            float angleX = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+
+
+            // Create the target rotation quaternion
+            Quaternion targetRotation = Quaternion.Euler(-angleX, 90f, 0f);
+
+            // Smoothly rotate the arm pivot to face the cursor position
+            armPivot.rotation = Quaternion.Slerp(armPivot.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+
+        }
+    }
 }
