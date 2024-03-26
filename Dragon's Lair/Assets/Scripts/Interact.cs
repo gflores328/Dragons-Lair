3ab9c149d6b3842ee02343ba2e693c4c7047d2b6
Gabriel Flores <gravy239@gmail.com>
2024-03-26 00:18:21 -0500

    Fixed Build Error

--- Dragon's Lair/Assets/Scripts/Interact.cs
+++ Dragon's Lair/Assets/Scripts/Interact.cs
@@ -12,11 +12,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 #if UNITY_EDITOR
 
 using UnityEditor;
-using UnityEngine.InputSystem;
+
 
 #endif
 
