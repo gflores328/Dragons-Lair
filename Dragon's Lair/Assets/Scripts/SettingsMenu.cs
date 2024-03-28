e10d9874eece28caac854c08c2e8a0ffdaf15842
jleatherman3 <77819727+jleatherman3@users.noreply.github.com>
2024-03-28 10:42:16 -0500

    Using a different method for the resolution now to give more options for screen resolution. Please test and let me know which ones to keep/discard.

--- Dragon's Lair/Assets/Scripts/SettingsMenu.cs
+++ Dragon's Lair/Assets/Scripts/SettingsMenu.cs
@@ -16,6 +16,7 @@
  * on Mac outside the editor so another team member will need to test full settings menu functions 
  * on other devices to check cross-compatibility and make sure resolutions match.
  * 
+ * Used this tutorial:  https://www.youtube.com/watch?v=JRnNqQ2wbOU
  * 
  */
 
@@ -42,345 +43,53 @@ public class SettingsMenu : MonoBehaviour
     //A float for storing the volume
     float volume;
 
+    List<int>widths = new List<int>() { 568, 800, 960, 1024, 1280, 1440, 1710, 2048, 2560, 2880 };
+    List<int> heights = new List<int>() { 320, 600, 600, 640, 800, 900, 1107, 1280, 1656, 1864};
 
-    //An array that stores a list of resolutions
-    //private Resolution[] resolutions;
-
-
-    private Resolution currentResolution;
-
-    // Fixed aspect ratio parameters
-    static public bool FixedAspectRatio = true;
-    static public float TargetAspectRatio = 4 / 3f;
-
-    // Windowed aspect ratio when FixedAspectRatio is false
-    static public float WindowedAspectRatio = 4f / 3f;
-
-    // List of horizontal resolutions to include
-    int[] resolutions = new int[] { 600, 800, 1024, 1280, 1400, 1600, 1920 };
-
-    public Resolution DisplayResolution;
-    public List<Vector2> WindowedResolutions, FullscreenResolutions;
-
-    int currWindowedRes, currFullscreenRes;
-
-    int currentResolutionIndex;
-
-    private void Awake()
+    public void SetScreenSize(int index)
     {
-        UnityEngine.EventSystems.EventSystem.current.SetSelectedGameObject(backButton);
+        bool fullscreen = Screen.fullScreen;
+        int width = widths[index];
+        int height = heights[index];
 
-    }
-    //This function is run when the scene starts
-    //Initalizes the options in the 'resolutionDropdown' dropdown box and the position of the volume slider
-    private void Start()
-    {
-        //Set up local variables
-
-
-        StartCoroutine(StartRoutine());
-
-
-        IEnumerator StartRoutine()
-        {
-
-            if (Application.platform == RuntimePlatform.OSXPlayer)
-            {
-                DisplayResolution = Screen.currentResolution;
-            }
-            else
-            {
-                if (Screen.fullScreen)
-                {
-                    Resolution r = Screen.currentResolution;
-                    Screen.fullScreen = false;
-
-                    yield return null;
-                    yield return null;
-
-                    DisplayResolution = Screen.currentResolution;
-
-                    Screen.SetResolution(r.width, r.height, true);
-
-                    yield return null;
-                }
-                else
-                {
-                    DisplayResolution = Screen.currentResolution;
-                }
-
-            }
-
-            InitResolutions();
-        }
-
+        Screen.SetResolution(width, height, fullscreen);
+        
     }
 
-
-    private void InitResolutions()
+    public void GetFullScreen(bool _fullscreen)
     {
-        float screenAspect = (float)DisplayResolution.width / DisplayResolution.height;
-
-        WindowedResolutions = new List<Vector2>();
-        FullscreenResolutions = new List<Vector2>();
-
-        foreach (int w in resolutions)
-        {
-            if (w < DisplayResolution.width)
-            {
-                // Adding resolution only if it's 20% smaller than the screen
-                if (w < DisplayResolution.width * 0.8f)
-                {
-                    Vector2 windowedResolution = new Vector2(w, Mathf.Round(w / (FixedAspectRatio ? TargetAspectRatio : WindowedAspectRatio)));
-                    if (windowedResolution.y < DisplayResolution.height * 0.8f)
-                        WindowedResolutions.Add(windowedResolution);
-
-                    FullscreenResolutions.Add(new Vector2(w, Mathf.Round(w / screenAspect)));
-                }
-            }
-        }
-
-        // Adding fullscreen native resolution
-        FullscreenResolutions.Add(new Vector2(DisplayResolution.width, DisplayResolution.height));
-
-        // Adding half fullscreen native resolution
-        Vector2 halfNative = new Vector2(DisplayResolution.width * 0.5f, DisplayResolution.height * 0.5f);
-        if (halfNative.x > resolutions[0] && FullscreenResolutions.IndexOf(halfNative) == -1)
-            FullscreenResolutions.Add(halfNative);
-
-        FullscreenResolutions = FullscreenResolutions.OrderBy(resolution => resolution.x).ToList();
-
-        bool found = false;
-
-        if (Screen.fullScreen)
-        {
-            currWindowedRes = WindowedResolutions.Count - 1;
-
-            for (int i = 0; i < FullscreenResolutions.Count; i++)
-            {
-                if (FullscreenResolutions[i].x == Screen.width && FullscreenResolutions[i].y == Screen.height)
-                {
-                    currFullscreenRes = i;
-                    found = true;
-                    break;
-                }
-            }
-
-            if (!found)
-                SetResolution(FullscreenResolutions.Count - 1, true);
-        }
-        else
-        {
-            currFullscreenRes = FullscreenResolutions.Count - 1;
-
-            for (int i = 0; i < WindowedResolutions.Count; i++)
-            {
-                if (WindowedResolutions[i].x == Screen.width && WindowedResolutions[i].y == Screen.height)
-                {
-                    found = true;
-                    currWindowedRes = i;
-                    break;
-                }
-            }
-
-            if (!found)
-                SetResolution(WindowedResolutions.Count - 1, false);
-        }
+        Screen.fullScreen = _fullscreen;
     }
 
-    public void SetResolution(int index, bool fullscreen)
-    {
-        Vector2 r = new Vector2();
-        if (fullscreen)
-        {
-            currFullscreenRes = index;
-            r = FullscreenResolutions[currFullscreenRes];
-        }
-        else
-        {
-            currWindowedRes = index;
-            r = WindowedResolutions[currWindowedRes];
-        }
+public void SetQuality(int qualityIndex)
+{
+      QualitySettings.SetQualityLevel(qualityIndex);
+}
 
-        bool fullscreen2windowed = Screen.fullScreen & !fullscreen;
+//Sets the volume of a specific chanel in the attached audio mixer equal to the given float value
+public void SetVolume(float volume)
+{
+//If the volume slider is at its minimum value, set the audio mixer to -80 db to properly mute the volume
+     if (volume == volumeSlider.minValue)
+     {
+          audioMixer.SetFloat("Master", -80);
+     }
+     else
+     {
+           audioMixer.SetFloat("Master", volume);
+     }
 
-        Debug.Log("Setting resolution to " + (int)r.x + "x" + (int)r.y);
-        Screen.SetResolution((int)r.x, (int)r.y, fullscreen);
+      //Update the default slider position to match the current audio mixer volume
 
-        // On OSX the application will pass from fullscreen to windowed with an animated transition of a couple of seconds.
-        // After this transition, the first time you exit fullscreen you have to call SetResolution again to ensure that the window is resized correctly.
-        if (Application.platform == RuntimePlatform.OSXPlayer)
-        {
-            // Ensure that there is no SetResolutionAfterResize coroutine running and waiting for screen size changes
-            StopAllCoroutines();
+      //Get the current audio volume from the audio mixer
+           audioMixer.GetFloat("Master", out volume);
 
-            // Resize the window again after the end of the resize transition
-            if (fullscreen2windowed) StartCoroutine(SetResolutionAfterResize(r));
-        }
-    }
-
-    private IEnumerator SetResolutionAfterResize(Vector2 r)
-    {
-        int maxTime = 5; // Max wait for the end of the resize transition
-        float time = Time.time;
-
-        // Skipping a couple of frames during which the screen size will change
-        yield return null;
-        yield return null;
-
-        int lastW = Screen.width;
-        int lastH = Screen.height;
-
-        //A list of screen resolution options saved as strings
-        List<string> options = new List<string>();
-        //Clears the placeholder options held by the attached dropdown box
-        resolutionDropdown.ClearOptions();
-
-
-        // Waiting for another screen size change at the end of the transition animation
-        while (Time.time - time < maxTime)
-        {
-            if (lastW != Screen.width || lastH != Screen.height)
-            {
-                string option = (Screen.width + "x" + Screen.height);
-                options.Add(option);
-                currentResolutionIndex = +1;
-
-                //If the selected resolution is the same as the current screen resolution, save the index of the selected resolution
-                if (Screen.width == Screen.currentResolution.width &&
-                    Screen.height == Screen.currentResolution.height)
-                {
-                    //Update the options in the attached dropdown box
-
-                    //Populate the attached dropdown box with the resolution option strings
-                    resolutionDropdown.AddOptions(options);
-                    //Set the attached dropdown box to display the option that matches the current screen resolution
-                    resolutionDropdown.value = currentResolutionIndex;
-                    //Refresh the attached dropdown box to show the option that matches the current screen resolution
-                    resolutionDropdown.RefreshShownValue();
-                }
-
-                //Debug.Log("End waiting");
-
-                Screen.SetResolution((int)r.x, (int)r.y, Screen.fullScreen);
-                yield break;
-            }
-
-            yield return null;
-        }
-
-    }
-
-
-
-        //Controls whether the game window is full screen based on the given true or false value
-        public void SetFullscreen(bool isFullscreen)
-        {
-            Screen.fullScreen = isFullscreen;
-        }
-
-
-        public void ToggleFullscreen()
-        {
-            SetResolution(
-                Screen.fullScreen ? currWindowedRes : currFullscreenRes,
-                !Screen.fullScreen);
-        }
-
-
-        public void SetQuality(int qualityIndex)
-        {
-            QualitySettings.SetQualityLevel(qualityIndex);
-        }
-
-        //Sets the volume of a specific chanel in the attached audio mixer equal to the given float value
-        public void SetVolume(float volume)
-        {
-            //If the volume slider is at its minimum value, set the audio mixer to -80 db to properly mute the volume
-            if (volume == volumeSlider.minValue)
-            {
-                audioMixer.SetFloat("Master", -80);
-            }
-            else
-            {
-                audioMixer.SetFloat("Master", volume);
-            }
-
-            //Update the default slider position to match the current audio mixer volume
-
-            //Get the current audio volume from the audio mixer
-            audioMixer.GetFloat("Master", out volume);
-            //Set the default slider value to the saved volume value
-            volumeSlider.value = volume;
-        }
+           //Set the default slider value to the saved volume value
+           volumeSlider.value = volume;
+      }
 
     
 }
-
-        /* currentResolution = Screen.currentResolution;
-
-         //Gets the list of resolutions supported by the current monitor
-         //resolutions = Screen.resolutions;
-
-
-         //A list of screen resolution options saved as strings
-         List<string> options = new List<string>();
-         //Clears the placeholder options held by the attached dropdown box
-         resolutionDropdown.ClearOptions();
-
-         //Convert all of the supported screen resolutions to strings for displaying in the attached dropdown box
-
-         //Used to automatically set dropdown box to the current screen resolution (once it's options have been populated)
-         int currentResolutionIndex = 0;
-         for (int i = 0; i < resolutions.Length; i++)
-         {
-             //Convert the selected resolution to a string
-             string option = resolutions[i].width + " x " + resolutions[i].height;
-             //Add the converted string to the list of resolution option strings
-             options.Add(option);
-
-             //If the selected resolution is the same as the current screen resolution, save the index of the selected resolution
-             if (resolutions[i].width == Screen.currentResolution.width &&
-                 resolutions[i].height == Screen.currentResolution.height)
-             {
-                 currentResolutionIndex = i;
-             }
-         }*/
-
-        //Update the options in the attached dropdown box
-
-        /*//Populate the attached dropdown box with the resolution option strings
-        resolutionDropdown.AddOptions(options);
-        //Set the attached dropdown box to display the option that matches the current screen resolution
-        resolutionDropdown.value = currentResolutionIndex;
-        //Refresh the attached dropdown box to show the option that matches the current screen resolution
-        resolutionDropdown.RefreshShownValue();*/
-
-
-
-
-        //Updates the current resolution of the game window based on the given resolution index
-        /*public void SetResolution(int resolutionIndex)
-        {
-            //Gets the resolution from the list of resolutions supported by the monitor
-            Resolution resolution = resolutions[resolutionIndex];
-            //Set the screen size to the selected resolution
-            Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
-        }
-
-        //Controls whether the game window is full screen based on the given true or false value
-        public void SetFullscreen(bool isFullscreen)
-        {
-            Screen.fullScreen = isFullscreen;
-        }*/
-
-        /*
-         * Controls the quality of the graphics based on the given quality index
-         * A list of quality options can be found at Edit > Project Settings > Quality
-         * 
-         * The list of quality options found on the dropdown box is NOT directly connected to the options found in the project settings
-         * Any adjustment to the options in project settings will have to be manually adjusted on the dropdown box
-         */
 
 
     
