6723f87803e405dba0d36c00aff67f0219221edc
jleatherman3 <77819727+jleatherman3@users.noreply.github.com>
2024-04-01 19:41:01 -0500

    script used to verify if a controller is connected and to get the controller name to be used with main menu settings controls page

---
+++ Dragon's Lair/Assets/Scripts/VerifyController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerifyController : MonoBehaviour
+{
+    //this script verifies that the controller is connected and prints out details of it to the user
+
+    private bool connected = false;
+
+    IEnumerator CheckForController()
+    {
+        while (true)
+        {
+            var controllers = Input.GetJoystickNames();
+
+            if ((!connected) && (controllers.Length > 0))
+            {
+                connected = true; //controller is connected
+                Debug.Log("Controlled connected successfully.");
+                Debug.Log(controllers);
+                
+            }
+            else if ((connected) && (controllers.Length == 0))
+            {
+                connected = false; //controller is not connected
+                Debug.Log("Controller disconnected.");
+                Debug.Log(controllers);
+
+            }
+
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+    void Awake()
+    {
+        StartCoroutine(CheckForController());
+    }
+}
