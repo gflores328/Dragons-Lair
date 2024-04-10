44beacc5f5c2e0dec1a8eed8aaaca7e320ea19db
carlosdmtz13 <carlosdmtz13@gmail.com>
2024-04-10 00:52:56 -0500

    Claw Game (WIP)
    - Set up the claw game
    - No coding scripts yet

--- Dragon's Lair/Assets/Scripts/ItemDescription.cs
+++ Dragon's Lair/Assets/Scripts/ItemDescription.cs
@@ -15,18 +15,6 @@ public class ItemDescription : MonoBehaviour, IPointerEnterHandler, IPointerExit
     public Item item;
     public TextMeshProUGUI descriptionBox;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     public void OnPointerEnter(PointerEventData pointerEventData)
     {
         descriptionBox.text = item.GetDescription();
