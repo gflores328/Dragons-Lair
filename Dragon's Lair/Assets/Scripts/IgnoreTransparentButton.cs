5b661aa7bfda4d26e8f8e9534ccfe4726d60177a
Gabriel Flores <gravy239@gmail.com>
2024-04-11 21:09:33 -0500

    Fixed mimic menu buttons

---
+++ Dragon's Lair/Assets/Scripts/IgnoreTransparentButton.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IgnoreTransparentButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Image>().alphaHitTestMinimumThreshold = 0.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
