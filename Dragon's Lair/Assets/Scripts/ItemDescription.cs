08f0938a66b2eda3aead674996f08221a662e726
carlosdmtz13 <carlosdmtz13@gmail.com>
2024-04-12 03:33:41 -0500

    Some Comments in the ItemDescription Script

--- Dragon's Lair/Assets/Scripts/ItemDescription.cs
+++ Dragon's Lair/Assets/Scripts/ItemDescription.cs
@@ -1,5 +1,5 @@
 /*
- * Created by: Carlos Martinez
+ * Created by: Carlos Martinez and Gabriel Flores
  *
  * Displays Item Description in the Inventory Menu When the Cursor is Pointing at an Item. 
  */
@@ -15,17 +15,20 @@ public class ItemDescription : MonoBehaviour, IPointerEnterHandler, IPointerExit
     public Item item;
     public TextMeshProUGUI descriptionBox;
 
+    // Displays Text from the Item's Description when the Mouse Cursor Points at it
     public void OnPointerEnter(PointerEventData pointerEventData)
     {
         descriptionBox.text = item.GetDescription();
         Debug.Log("Mouse in image");
     }
 
+    // Clears Text when the Mouse Cursor No Longer Points at the Item
     public void OnPointerExit(PointerEventData pointerEventData)
     {
         descriptionBox.text = null;
     }
 
+    // Sets an Item
     public void SetItem(Item takenItem)
     {
         item = takenItem;
