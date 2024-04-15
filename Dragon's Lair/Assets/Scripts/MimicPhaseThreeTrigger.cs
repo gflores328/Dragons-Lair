2e40a911507c5c1468fa5ad1fbf041a3ebe7c36f
Gabriel Flores <gravy239@gmail.com>
2024-04-15 18:46:40 -0500

    Boss 2 and 3 adjusted

--- Dragon's Lair/Assets/Scripts/MimicPhaseThreeTrigger.cs
+++ Dragon's Lair/Assets/Scripts/MimicPhaseThreeTrigger.cs
@@ -1,17 +1,8 @@
-/*
- * Crated By: Gabriel Flores
- * 
- * This script will hold the start trigger for the second boss to start
- */
-
-
-
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MimicPhaseTwoTrigger : MonoBehaviour
+public class MimicPhaseThreeTrigger : MonoBehaviour
 {
     public GameObject entrance;
     public GameObject mimic;
@@ -22,7 +13,7 @@ public class MimicPhaseTwoTrigger : MonoBehaviour
         //entrance.SetActive(true);
         if (other.tag == "Player")
         {
-            mimic.GetComponent<MimicPhaseTwo>().start = true;
+            mimic.GetComponent<MimicPhaseThree>().start = true;
             healthBar.SetActive(true);
 
             Destroy(gameObject);
