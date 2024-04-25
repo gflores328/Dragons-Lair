e0adf077ef0923b28e7439384be17035246e5df4
jleatherman3 <77819727+jleatherman3@users.noreply.github.com>
2024-04-25 14:36:11 -0500

    Added lineofsight script for kobolds so that they will use raycast to detect player in range and start moving towards him.

---
+++ Dragon's Lair/Assets/Scripts/LineofSight.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineofSight : MonoBehaviour
+{
+    public float TargetDistance; // Distace player is before enemy starts action ie walking or attacking
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit; //sends out a raycast from orgin in desired direction until it hits something
+
+        if (Physics.Raycast (transform.position,transform.TransformDirection (Vector3.forward), out hit)) //raycast continues in the direction forward until it hits something
+        {
+            TargetDistance = hit.distance; //determines how far the ray can go.
+        }
+    }
+}
