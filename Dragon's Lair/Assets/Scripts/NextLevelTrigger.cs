3b41b3749051267fc26f0fc12752f0ba26fc2f59
Gabriel Flores <gravy239@gmail.com>
2024-04-28 16:24:40 -0500

    Glitch effect after forest 2

--- Dragon's Lair/Assets/Scripts/NextLevelTrigger.cs
+++ Dragon's Lair/Assets/Scripts/NextLevelTrigger.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class NextLevelTrigger : MonoBehaviour
 {
@@ -8,17 +9,38 @@ public class NextLevelTrigger : MonoBehaviour
 
     [SerializeField] private string levelToLoad;
     [SerializeField] private GameObject LoadingScreen;
-    
+
+    public bool glitchScreen = false;
+    public GameObject mainCamera;
+    public GameObject glitchCamera;
 
     void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
-            Time.timeScale = 0;
-            gameManager.LoadSceneAsync(levelToLoad);
+            if (glitchScreen)
+            {
+                other.GetComponent<PlayerInput>().actions.Disable();
+                StartCoroutine(GlitchAndLoad());
+            }
+            else
+            {
+                Time.timeScale = 0;
+                gameManager.LoadSceneAsync(levelToLoad);
+            }
         }
         
     }
 
+    IEnumerator GlitchAndLoad()
+    {
+        mainCamera.SetActive(false);
+        glitchCamera.SetActive(true);
+
+
+        yield return new WaitForSeconds(3f);
+
+        gameManager.LoadSceneAsync(levelToLoad);
+    }
 
 }
